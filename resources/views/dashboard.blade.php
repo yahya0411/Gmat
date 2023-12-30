9b499dba09b83a3e1d255422bf05baeb388207c7
Yahia Mekki <mekki.yahia0411@gmail.com>
2023-12-30 19:11:08 +0100

    fixing some issues

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,9 +1,22 @@
 @extends('app.master')
 @section('content')
-
+<div class="row">
+    <div class="col-md-10 offset-md-1">
+        <form action="simple-results.html">
+        <div class="input-group">
+        <input type="search" class="form-control form-control-lg" placeholder="Taper Bureau de poste">
+        <div class="input-group-append">
+        <button type="submit" class="btn btn-lg btn-default">
+        <i class="fa fa-search"></i>
+        </button>
+        </div>
+        </div>
+        </form>
+        </div>
+</div>
 <div class="row">
     <div class="col-md-3 col-sm-6 col-12">
-      <div class="info-box bg-gradient-Lightblue">
+      <div class="info-box bg-gradient-Primary">
         <span class="info-box-icon"><img src="{{asset('icons/pc3.png')}}" alt=""></span>
 
         <div class="info-box-content">
@@ -23,11 +36,30 @@
     </div>
     <!-- /.col -->
     <div class="col-md-3 col-sm-6 col-12">
-      <div class="info-box bg-gradient-Teal">
-        <span class="info-box-icon"><img src="{{asset('icons/printer.png')}}" alt=""></span>
+        <div class="info-box bg-gradient-Secondary">
+          <span class="info-box-icon"><img src="{{asset('icons/printer.png')}}" alt=""></span>
+
+          <div class="info-box-content">
+            <span class="info-box-text">Imprimante</span>
+            <span class="info-box-number">41,410</span>
+
+            <div class="progress">
+              <div class="progress-bar" style="width: 70%"></div>
+            </div>
+            <span class="progress-description">
+              70% Increase in 30 Days
+            </span>
+          </div>
+          <!-- /.info-box-content -->
+        </div>
+        <!-- /.info-box -->
+      </div>
+    <div class="col-md-3 col-sm-6 col-12">
+      <div class="info-box bg-gradient-Info">
+        <span class="info-box-icon"><img src="{{asset('icons/Reader.png')}}" alt=""></span>
 
         <div class="info-box-content">
-          <span class="info-box-text">Imprimante</span>
+          <span class="info-box-text">Lecteurs</span>
           <span class="info-box-number">41,410</span>
 
           <div class="progress">
@@ -43,7 +75,7 @@
     </div>
     <!-- /.col -->
     <div class="col-md-3 col-sm-6 col-12">
-      <div class="info-box bg-gradient-Navy">
+      <div class="info-box bg-gradient-Success">
         <span class="info-box-icon"><img src="{{asset('icons/tpe.png')}}" alt=""></span>
 
         <div class="info-box-content">
@@ -62,28 +94,11 @@
       <!-- /.info-box -->
     </div>
     <!-- /.col -->
-    <div class="col-md-3 col-sm-6 col-12">
-      <div class="info-box bg-gradient-danger">
-        <span class="info-box-icon"><img src="{{asset('icons/network.png')}}" alt=""></span>
 
-        <div class="info-box-content">
-          <span class="info-box-text">Réseau</span>
-          <span class="info-box-number">41,410</span>
-
-          <div class="progress">
-            <div class="progress-bar" style="width: 70%"></div>
-          </div>
-          <span class="progress-description">
-            70% Increase in 30 Days
-          </span>
-        </div>
-        <!-- /.info-box-content -->
-      </div>
-      <!-- /.info-box -->
-    </div>
     <!-- /.col -->
   </div>
   <div class="row">
+
    <!-- <div class="col-md-8">
 
         <div class="card">
@@ -232,8 +247,8 @@
 
             </div>
     </div>-->
-  <div class="col-md-6">
-    <div class="card card-success">
+  <div class="col-md-9">
+    <div class="card card-default">
         <div class="card-header">
         <h3 class="card-title">Bar Chart</h3>
         <div class="card-tools">
@@ -247,39 +262,53 @@
         </div>
         <div class="card-body">
         <div class="chart"><div class="chartjs-size-monitor"><div class="chartjs-size-monitor-expand"><div class=""></div></div><div class="chartjs-size-monitor-shrink"><div class=""></div></div></div>
-        <canvas id="barChart" style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%; display: block; width: 765px;" class="chartjs-render-monitor" width="765" height="250"></canvas>
+        <canvas id="barChart" style="min-height: 250px; height: 200px; max-height: 200px; max-width: 100%; display: block; width: 765px;" class="chartjs-render-monitor" width="765" height="200"></canvas>
         </div>
         </div>
 
         </div>
   </div>
+  <div class="col-md-3 col-sm-6 col-12">
+    <div class="info-box bg-gradient-Orange">
+        <span class="info-box-icon"><img src="{{asset('icons/barecode.png')}}" alt=""></span>
+
+        <div class="info-box-content">
+          <span class="info-box-text">Douchette</span>
+          <span class="info-box-number">41,410</span>
+
+          <div class="progress">
+            <div class="progress-bar" style="width: 70%"></div>
+          </div>
+          <span class="progress-description">
+            70% Increase in 30 Days
+          </span>
+        </div>
+        <!-- /.info-box-content -->
+      </div>
+    <div class="info-box bg-gradient-Danger">
+      <span class="info-box-icon"><img src="{{asset('icons/network.png')}}" alt=""></span>
+
+      <div class="info-box-content">
+        <span class="info-box-text">Réseau</span>
+        <span class="info-box-number">41,410</span>
+
+        <div class="progress">
+          <div class="progress-bar" style="width: 70%"></div>
+        </div>
+        <span class="progress-description">
+          70% Increase in 30 Days
+        </span>
+      </div>
+      <!-- /.info-box-content -->
+    </div>
+    <!-- /.info-box -->
+
+  </div>
 
 </div>
-<script src="{{asset("plugins/jquery/jquery.min.js")}}"></script>
-<script src="{{asset('plugins/chart.js/Chart.min.js')}}"></script>
+<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
-<script>
-  $(function () {
-    'use strict'
-var barChartCanvas = $('#barChart').get(0).getContext('2d')
-    var barChartData = $.extend(true, {}, areaChartData)
-    var temp0 = areaChartData.datasets[0]
-    var temp1 = areaChartData.datasets[1]
-    barChartData.datasets[0] = temp1
-    barChartData.datasets[1] = temp0
 
-    var barChartOptions = {
-      responsive              : true,
-      maintainAspectRatio     : false,
-      datasetFill             : false
-    }
-
-    new Chart(barChartCanvas, {
-      type: 'bar',
-      data: barChartData,
-      options: barChartOptions
-    })
-
-})
-</script>
+<script src="/myjs.js"></script>
 @endsection
